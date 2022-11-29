1e2361599263088dee6542fa0119b30afa73e0f2
Sandra <90846642+sandrasansand@users.noreply.github.com>
2022-11-30 00:02:57 +0100

    update

--- appphp/tienda/conexion.php
+++ appphp/tienda/conexion.php
@@ -6,12 +6,7 @@ function conectar_db(){
     $pass = "";
     $bd = "tienda";
 	
-/*
-$server ="db738041399.db.1and1.com";
-$user = "dbo738041399";
-$pass = "Desarrolloweb123.";
-$bd= "db738041399";
-*/
+
 
     $conexion = mysqli_connect($server, $user, $pass,$bd) 
         or die("Ha sucedido un error inexperado en la conexion de la base de datos". mysqli_connect_error());
