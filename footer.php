4eaa0b9ee0173152ddb8e5d7dfde634fbeb423d5
sandrasansand <sandrasaezcarbonell@gmail.com>
2023-05-03 18:17:09 +0200

    first commit add footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="pie">
+
+        <ul>
+            <li><a href=/acerca-de>Acerca de</a></li>
+            <li><a href=/conectar>Conectar</a></li>
+            <li><a href=/rrss>Redes Sociales</a></li>
+        </ul>
+        <p id="copy">
+            &copy; Todos los derechos reservados Art Gallery 2021
+        </p>
+    </div>
+</footer>
